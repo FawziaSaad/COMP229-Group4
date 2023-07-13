98fe427bf8ebfa20316cc9f212894c7094648522
deschi0000 <79345657+deschi0000@users.noreply.github.com>
2023-07-13 17:18:48 +0800

    Add index Auth and create survey routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,23 @@ var router = express.Router();
 
 let indexController = require('../controllers/index');
 
+// helper function for guard purposes
+function requireAuth(req, res, next)
+{
+    // check if the user is logged in
+    if(!req.isAuthenticated())
+    {
+        return res.redirect('/login');
+    }
+    next();
+}
+
 // TODO: Move survey list into the landing page
 // TODO: Finish Survey model/schema
 // TODO: Populate dummy data on Atlas   
 // TODO: - READ functionality
+
+
 
 
 /* GET home page. */
@@ -14,6 +27,12 @@ router.get('/', indexController.displayHomePage);
 
 /* GET home page. */
 router.get('/home', indexController.displayHomePage);
+
+/* GET create survey page. */
+router.get('/create-survey', requireAuth, indexController.displayCreateSurvey);
+
+// POST Route for processing the Create Survey Page - CREATE Operation
+router.post('/create-survey', requireAuth, indexController.displayCreateSurvey);
 
 
 // Leave the login / logout
