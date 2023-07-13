acafa7a974cfffd69eb00de94716055f83c8daab
FawziaSaad <fawziaanime@gmail.com>
2023-07-12 21:49:52 -0400

    Remove about

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -11,10 +11,6 @@ module.exports.displayHomePage = (req, res, next) => {
     res.render('index', { title: 'Home', displayName: req.user ? req.user.displayName : '' });
 }
 
-module.exports.displayAboutPage = (req, res, next) => {
-    res.render('index', { title: 'About', displayName: req.user ? req.user.displayName : '' });
-}
-
 module.exports.displayProductsPage = (req, res, next) => {
     res.render('index', { title: 'Products', displayName: req.user ? req.user.displayName : '' });
 }
