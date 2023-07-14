4adf3e3d0e45328170ce66e280e163b01af6e584
Balkesa Azimi <a.asmaazimi@gmail.com>
2023-07-14 15:27:53 -0400

    Survey page edit

---
+++ public/Scripts/surveys/takeSurvey.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var surveyForm = document.getElementById("surveyForm");
+    surveyForm.addEventListener("submit", function (e) {
+      e.preventDefault();
+     
+    });
+  });
+  
