e45f39763e4386f8e0f40c87f234eed48bf0cfc0
deschi0000 <79345657+deschi0000@users.noreply.github.com>
2023-07-20 12:33:08 +0800

    add route data

--- client/src/app/partials/base-page/base-page.component.ts
+++ client/src/app/partials/base-page/base-page.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-base-page',
   templateUrl: './base-page.component.html',
   styleUrls: ['./base-page.component.css']
 })
-export class BasePageComponent {
+export class BasePageComponent implements OnInit {
+  title: string;
+
+  constructor(private route: ActivatedRoute) { }
+  ngOnInit(): void {
+    this.title = this.route.snapshot.data['title'];
+  }
 
 }
