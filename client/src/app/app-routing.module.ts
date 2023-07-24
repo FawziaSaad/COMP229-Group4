ff8d726dde7db599781a2d9e02f2a2f0fe79721e
mkazemicent <117585679+mkazemicent@users.noreply.github.com>
2023-07-24 18:31:25 -0400

    Compiles successfully. problem with the routing.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { SurveyComponent } from './pages/survey/survey.component';
 import { ErrorComponent } from './pages/error/error.component';
 import { CreateComponent } from './pages/surveys/create/create.component';
 import { MysurveysComponent } from './pages/surveys/mysurveys/mysurveys.component';
+import { SurveySiteComponent } from './survey-site/survey-site.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, data: {title: 'Recent Surveys'}},
   { path: '', component: HomeComponent, data: {title: 'Recent Surveys'}},
   { path: 'survey', component: SurveyComponent, data: {title: 'Survey'}},
   { path: 'error', component: ErrorComponent, data: {title: 'Error'}},
+  { path: 'survey-site', component: SurveySiteComponent, data: {title: 'Survey Site'}},
   { path: 'create-survey', component: CreateComponent, data: {title: 'Create Survey'}},
   { path: 'my-survey', component: MysurveysComponent, data: {title: 'My Survey'}}
 
