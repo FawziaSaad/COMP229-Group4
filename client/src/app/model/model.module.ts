77067382cc8d5c1636a2a8864c847abb4e55e86c
deschi0000 <79345657+deschi0000@users.noreply.github.com>
2023-07-29 17:45:02 +0800

    update model module

--- client/src/app/model/model.module.ts
+++ client/src/app/model/model.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from "@angular/core";
 import { SurveyRepository } from "./survey.repository";
 import { StaticDataSource } from "./static.datasource";
+import { HttpClientModule } from "@angular/common/http";
+import { RestDataSource } from "./rest.datasource";
 
 @NgModule({
-    providers: [SurveyRepository, StaticDataSource]
+    imports: [HttpClientModule],
+    providers: [SurveyRepository, StaticDataSource,
+        {provide: StaticDataSource, useClass: RestDataSource}]
 })
 export class ModelModules {}
