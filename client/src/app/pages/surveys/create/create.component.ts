d1f362566a5fa8d6b72409bf73a167262467abb2
deschi0000 <79345657+deschi0000@users.noreply.github.com>
2023-07-31 10:58:53 +0800

    create component

--- client/src/app/pages/surveys/create/create.component.ts
+++ client/src/app/pages/surveys/create/create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BasePageComponent } from '../../../partials/base-page/base-page.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-create',
@@ -8,10 +9,45 @@ import { BasePageComponent } from '../../../partials/base-page/base-page.compone
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent extends BasePageComponent implements OnInit{
-  constructor(route: ActivatedRoute){
+  
+  questionLength : number[] = new Array(5);
+  MCQoptions : number[] = new Array(4);
+  selectedSurveyType: string = '';
+  formData: any = {};
+
+  
+  constructor(route: ActivatedRoute, private http : HttpClient){
     super(route)
   }
+  
   override ngOnInit(): void {
     
   }
+
+  onSurveyTypeChange() {
+    // You can perform any additional logic here if needed when the selection changes.
+    // For example, you could reset form fields or perform some action based on the selected value.
+  }
+
+  onSubmit() {
+    const postData = {
+      questions: this.formData
+    };
+    this.http.post('YOUR_API_ENDPOINT', postData)
+      .subscribe(
+        (response) => {
+          console.log('POST request successful:', response);
+          // Handle the response as needed
+        },
+        (error) => {
+          console.error('Error occurred:', error);
+          // Handle errors if any
+        }
+      );
+
+
+
+  }
+
+
 }
