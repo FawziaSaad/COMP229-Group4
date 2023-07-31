841d8623a19d373fbe363b4205f1731f6fcdb875
deschi0000 <79345657+deschi0000@users.noreply.github.com>
2023-07-31 12:00:27 +0800

    create mysurveys component

--- client/src/app/pages/surveys/mysurveys/mysurveys.component.ts
+++ client/src/app/pages/surveys/mysurveys/mysurveys.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { BasePageComponent } from '../../../partials/base-page/base-page.component';
-import { ActivatedRoute } from '@angular/router';
-
+import { SurveyRepository } from 'src/app/model/survey.repository';
+import { Survey } from 'src/app/model/survey.model';
 
 @Component({
   selector: 'app-mysurveys',
   templateUrl: './mysurveys.component.html',
   styleUrls: ['./mysurveys.component.css']
 })
-export class MysurveysComponent extends BasePageComponent implements OnInit{
-  constructor(route: ActivatedRoute){
-    super(route)
+export class MysurveysComponent implements OnInit {
+
+  displayName: string = '';
+  title: string = 'My Surveys';
+  mySurveys: Survey[] = []; // Changed property name to mySurveys
+
+  constructor(private repository: SurveyRepository) {}
+
+  ngOnInit(): void {
+    this.loadSurveys();
   }
-  override ngOnInit(): void {
-    
+
+  loadSurveys() {
+    this.mySurveys = this.repository.getSurveys();
   }
 }
