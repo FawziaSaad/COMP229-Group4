86a96fa1247eaad8e73ebdc450f6f96247466385
deschi0000 <79345657+deschi0000@users.noreply.github.com>
2023-07-31 12:00:10 +0800

    try to implement auth service

--- client/src/app/partials/header/header.component.ts
+++ client/src/app/partials/header/header.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/tempAuthService';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  displayName: string = '';
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.displayName = this.authService.getCurrentUser();
+  }
 }
