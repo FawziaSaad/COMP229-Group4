4c41819b3ada947ff3862994de9cc491c6f854f6
deschi0000 <79345657+deschi0000@users.noreply.github.com>
2023-07-31 18:48:35 +0800

    take survery component

--- client/src/app/pages/survey/survey.component.ts
+++ client/src/app/pages/survey/survey.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BasePageComponent } from '../../partials/base-page/base-page.component';
+import { Survey } from 'src/app/model/survey.model';
 
 
 @Component({
@@ -9,10 +10,25 @@ import { BasePageComponent } from '../../partials/base-page/base-page.component'
   styleUrls: ['./survey.component.css']
 })
 export class TakeSurveyComponent extends BasePageComponent implements OnInit{
+
+  surveyToTake: Survey | undefined;
+
   constructor(route: ActivatedRoute){
     super(route)
   }
   override ngOnInit(): void {
-    
+    const id = this.route.snapshot.params['id'];
+    this.getSurveyById(id);
   }
+
+  getSurveyById(id: string): void {
+    // Implement the logic to fetch the survey data using Angular's HttpClient or any other method.
+    // Assign the survey data to the surveyToTake property.
+  }
+
+
+  onSubmit(): void {
+    // Implement the logic to handle form submission when the "Submit Survey" button is clicked.
+  }
+
 }
