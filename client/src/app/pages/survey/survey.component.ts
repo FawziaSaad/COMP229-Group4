342ac35ebfa44210b59fb6c6a32315a65dbe4aa7
deschi0000 <79345657+deschi0000@users.noreply.github.com>
2023-08-01 14:14:16 +0800

    post logic for taking survey

--- client/src/app/pages/survey/survey.component.ts
+++ client/src/app/pages/survey/survey.component.ts
@@ -1,9 +1,9 @@
 // survey-take.component.ts
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Survey } from 'src/app/model/survey.model';
 import { SurveyRepository } from 'src/app/model/survey.repository';
-
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-survey',
@@ -14,10 +14,13 @@ export class TakeSurveyComponent implements OnInit {
   selectedAnswers: any[] = [];
   MCQoptions : number[] = new Array(4);
   id: any = this.route.snapshot.paramMap.get('id');
+  currentSurvey : Survey;
 
   constructor(
     private surveyRepository: SurveyRepository,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -26,12 +29,49 @@ export class TakeSurveyComponent implements OnInit {
 
   get surveyToTake() {
     // this.surveyToTake = this.surveyRepository.getSurveyById(this.id);
-    return  this.surveyRepository.getSurveyById(this.id);
+
+    this.currentSurvey = this.surveyRepository.getSurveyById(this.id);
+    return this.currentSurvey;
   }
 
-  submitSurvey(){
-    // What happens when we submit
+  
+  submitSurvey(data){    
+    // console.log(this.currentSurvey.questions)
+    
+        let questions = [];
+        let responses = [this.selectedAnswers];
+
+    //push the questions in the question array
+    for (let q of this.currentSurvey.questions) {
+      questions.push(q.Question);
+    }
+
+    // Check the arrays
+    console.log('//=============================================');
+    console.log(questions);
+    console.log(responses[0])
+    console.log('//=============================================');
+
+    let responseToSend = {
+      surveyId: this.id,
+      respondentId: "1", // the req.user.id -> current user will go here
+      takenBy: "Moh", //req.user.displayName -> Same for this one
+      questions: questions,
+      responses: responses[0]
+    };
+
+    console.log(responseToSend)
+    try {
+      this.http.post(`http://localhost:3000/survey/${this.id}`, responseToSend).subscribe(
+        (response) => {
+          console.log('SA Form submitted:', response);
+          // Handle the response as needed
+        }
+      );
+    }
+    catch (err) {
+      console.log(err);
+    }
+    this.router.navigate(['/']);
   }
-
-
 }
