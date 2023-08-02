2809d27af42e858298d29335c8d28a08f1a38c3f
tsbillington <tsbillington4@gmail.com>
2023-08-02 16:50:21 -0400

    update for Moh

--- client/src/app/pages/surveys/edit/edit.component.ts
+++ client/src/app/pages/surveys/edit/edit.component.ts
@@ -16,96 +16,100 @@ import { Survey } from 'src/app/model/survey.model';
 import { SurveyRepository } from 'src/app/model/survey.repository';
 import { HttpClient} from '@angular/common/http';
 
+// from create survey
+
+import { Component, OnInit, ViewChild, ElementRef  } from '@angular/core';
+//import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Component({
   selector: 'app-survey',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css'],
 })
 export class EditComponent implements OnInit {
-  selectedAnswers: any[] = [];
-  MCQoptions : number[] = new Array(4);
-  id: any = this.route.snapshot.paramMap.get('id');
-  currentSurvey : Survey;
+  mcqContainerDisplay = 'none';
+  saContainerDisplay = 'none';
+  @ViewChild ('surveyType') surveyType: ElementRef;
 
-  constructor(
-    private surveyRepository: SurveyRepository,
-    private route: ActivatedRoute,
-    private http: HttpClient,
-    private router: Router
-  ) {}
-
-  ngOnInit(): void {
-
-  }
-
-  get surveyToEdit() {
-    // this.surveyToTake = this.surveyRepository.getSurveyById(this.id);
-
-    this.currentSurvey = this.surveyRepository.getSurveyById(this.id);
-    return this.currentSurvey;
-  }
-
-
-  // submitSurvey(data){
-  //   // console.log(this.currentSurvey.questions)
-
-  //       let questions = [];
-  //       //let responses = [this.selectedAnswers];
-
-  //   //push the questions in the question array
-  //   for (let q of this.currentSurvey.questions) {
-  //     questions.push(q.Question);
-  //   }
-
-    // // Check the arrays
-    // console.log('//=============================================');
-    // console.log(questions);
-    // console.log(responses[0])
-    // console.log('//=============================================');
-
-    // let responseToSend = {
-    //   surveyId: this.id,
-    //   respondentId: "1", // the req.user.id -> current user will go here
-    //   takenBy: "Moh", //req.user.displayName -> Same for this one
-    //   questions: questions,
-    //   responses: responses[0]
-    // };
-
-    // console.log("Response to send:");
-    // console.log(responseToSend)
-    try {
-      this.http.post(`http://localhost:3000/survey/${this.id}`, responseToSend).subscribe(
-        (response) => {
-          console.log('Responses:', response);
-          // Handle the response as needed
-        }
-      );
-    }
-    catch (err) {
-      console.log(err);
-    }
-    this.router.navigate(['/']);
+constructor(private router: Router, private http: HttpClient, private repository: SurveyRepository) { }
+onSurveyTypeChange(surveyType: string) {
+  if (surveyType === 'MCQ') {
+    console.log('It is a Multiple Choice');
+    this.mcqContainerDisplay = 'block';
+    this.saContainerDisplay = 'none';
+  } else if (surveyType === 'SA') {
+    console.log('It is a Short Answer Survey');
+    this.mcqContainerDisplay = 'none';
+    this.saContainerDisplay = 'block';
   }
 }
 
+get surveys(): Survey[] {
 
-// GET ROUTE FOR EDITING A SURVEY
-module.exports.displayEditSurvey = async (req, res, next) => {
-  let id = req.params.id;
+  const id: string = 	"64b04fdc4038f37b48c37ce7";
+  return this.repository.getSurveys().filter((survey) => survey.userid === id);
+}
+
+onSubmit1(data) {
+  // console.log(JSON.stringify(data) + this.surveyType.nativeElement.value);
+  // POST METHOD GO HERE TO CREATE SURVEY IN SERVER
+  let surveyToSend = {
+    surveyName: data.surveyName,
+    surveyType: this.surveyType.nativeElement.value,
+    Question1: String,
+    Question2: String,
+    response1: [],
+    response2: []
+  };
+  for (let i = 0; i < 2; i++) {
+    surveyToSend[`Question${i+1}`] = data[`Question${i + 1}`];
+    for (let j = 0; j < 4; j++) {
+      surveyToSend[`response${i+1}`].push(data[`response${i+1}${j+1}`]);
+    }
+  }
 
   try {
-      let surveyToEdit = await Surveys.findById(id);
-      // res.render('surveys/edit',
-      // {title: 'Edit',
-      // survey: surveyToEdit,
-      // displayName: req.user ? req.user.displayName : ''});
-      res.json({
-          title: 'Edit',
-          survey: surveyToEdit,
-          displayName: req.user ? req.user.displayName : ''
-      });
-  } catch (err){
-      console.log(err);
-      res.status(500).send(err);
+    this.http.post('http://localhost:3000/create-survey', surveyToSend).subscribe(
+      (response) => {
+        console.log('SA Form submitted:', response);
+        // Handle the response as needed
+      }
+    );
   }
-};
+  catch (err) {
+    console.log(err);
+  }
+  this.router.navigate(['/']);
+}
+
+onSubmit2(data) {
+  // console.log(JSON.stringify(data) + this.surveyType.nativeElement.value);
+  // POST METHOD GO HERE TO CREATE SURVEY IN SERVER
+
+  let surveyToSend = {
+    surveyName: data.surveyName,
+    surveyType: this.surveyType.nativeElement.value,
+    Question1: String,
+    Question2: String
+  }
+  for (let i = 0; i < 2; i++) {
+     surveyToSend[`Question${i+1}`] = data[`Question${i + 1}`];
+  }
+
+  try {
+    this.http.post('http://localhost:3000/create-survey', surveyToSend).subscribe(
+      (response) => {
+        console.log('SA Form submitted:', response);
+        // Handle the response as needed
+      });
+  }
+  catch (err) {
+    console.log(err);
+  }
+  this.router.navigate(['/']);
+}
+ngOnInit(): void {
+}
+
+}
