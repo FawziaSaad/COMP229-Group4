e1c93e03569210c916b7efb507c0a815d8271355
deschi0000 <79345657+deschi0000@users.noreply.github.com>
2023-08-11 09:59:08 +0800

    add authgaurd to routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { TakeSurveyComponent } from './pages/survey/survey.component';
 ///// tim added below line
 import { EditComponent } from './pages/surveys/edit/edit.component';
 import { ReportComponent } from './pages/surveys/report/report.component';
+import { AuthGuard } from './admin/auth/auth.guard';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, data: {title: 'Recent Surveys'}},
@@ -17,14 +18,14 @@ const routes: Routes = [
   { path: 'login', data: {title: 'Login'}, redirectTo: '/admin/auth', pathMatch: 'full'},
   { path: 'error', component: ErrorComponent, data: {title: 'Error'}},
   { path: 'survey-site', component: SurveySiteComponent, data: {title: 'Survey Site'}},
-  { path: 'create-survey', component: CreateComponent, data: {title: 'Create Survey'}},
-  { path: 'survey/mysurveys', component: MysurveysComponent, data: {title: 'My Surveys'}},
+  { path: 'create-survey', component: CreateComponent, data: {title: 'Create Survey'}, canActivate: [AuthGuard]},
+  { path: 'survey/mysurveys', component: MysurveysComponent, data: {title: 'My Surveys'}, canActivate: [AuthGuard]},
   { path: 'survey/:id', component: TakeSurveyComponent, data: {title: 'Take Survey'}},
-  { path: 'survey/delete/:id', component: MysurveysComponent},
-  { path: 'survey/report/:id', component: ReportComponent, data: { title: 'Survey Report' } },
+  { path: 'survey/delete/:id', component: MysurveysComponent, canActivate: [AuthGuard]},
+  { path: 'survey/report/:id', component: ReportComponent, data: { title: 'Survey Report' }, canActivate: [AuthGuard]},
   ///// tim added below line
-  { path: 'survey/edit/:id', component: EditComponent, data: {title: 'Edit Survey'}},
-  // {path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'survey/edit/:id', component: EditComponent, data: {title: 'Edit Survey'}, canActivate: [AuthGuard]},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: '**', redirectTo: '/home', pathMatch: 'full'}, // Needs to be here for the logout redirect in header!
   {path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)},
 
